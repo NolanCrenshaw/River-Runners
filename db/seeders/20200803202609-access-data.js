6971ced0a6090299c43c5da26e014197dca42196
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-08-04 18:53:32 -0400

    seeder file bug fix

--- db/seeders/20200803202609-access-data.js
+++ db/seeders/20200803202609-access-data.js
@@ -31,6 +31,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Accessess', null, {})
+    return queryInterface.bulkDelete('Accesses', null, {})
   }
 };
