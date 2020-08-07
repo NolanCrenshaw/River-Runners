20504e6a2cae43ea5d83725b66ef31dcbd8ff352
NolanCrenshaw <nolan.crenshaw@gmail.com>
2020-08-07 17:36:41 -0400

    bug fix src/config

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 export const imageUrl = 
     process.env.REACT_APP_IMAGE_URL ||
     process.env.NODE_ENV === "production"
-        ? "https://river-runners.herokuapp.com/"
+        ? "https://river-runners.herokuapp.com"
         : "http://localhost:8080";
 
 export const baseUrl = process.env.REACT_APP_BASEURL || `${imageUrl}/api`;
